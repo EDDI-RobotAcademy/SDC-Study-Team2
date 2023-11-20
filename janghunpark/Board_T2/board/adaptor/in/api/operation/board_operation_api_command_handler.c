96b94118ac3c68002820e13cdfb51e601cfb64cf
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2023-11-20 17:02:24 +0900

    update: [박장훈] 입력한 데이터를  post_info에 저장됨 확인 [SDC-T2-x]

--- janghunpark/Board_T2/board/adaptor/in/api/operation/board_operation_api_command_handler.c
+++ janghunpark/Board_T2/board/adaptor/in/api/operation/board_operation_api_command_handler.c
@@ -16,18 +16,18 @@
 
 void board_operation(void)
 {
-    post_info *post_inforamtion;
-    int command_number = get_command_number_from_user(); // int type의 command index를 받는 함수
+    // post_info *post_inforamtion;
+    // int command_number = get_command_number_from_user(); // int type의 command index를 받는 함수
 
-    // 아래 정보들을 입력받는 함수 ; get_info 혹은 read_info
-    char *title;
-    char *writer;
-    char *contents;
+    // // 아래 정보들을 입력받는 함수 ; get_info 혹은 read_info
+    // char *title;
+    // char *writer;
+    // char *contents;
 
-    post_inforamtion = init_post_info_by_write_command(); // 괄호 안에 info에 대한 함수를 받아 post로 도출
+    // post_inforamtion = init_post_info_by_write_command(); // 괄호 안에 info에 대한 함수를 받아 post로 도출
 
-    printf("입력하신 기능을 수행합니다!\n");
+    // printf("입력하신 기능을 수행합니다!\n");
 
-    // post에 대해 사용자가 입력한 명령 실행
-    board_service_command_table[SERVICE_ON_BOARD_EXECUTE_BUFFER_COUNT](post_inforamtion);
+    // // post에 대해 사용자가 입력한 명령 실행
+    // board_service_command_table[SERVICE_ON_BOARD_EXECUTE_BUFFER_COUNT](post_inforamtion);
 }
--- janghunpark/Board_T2/board/utility/ui/console/user_keyboard_input.h
+++ janghunpark/Board_T2/board/utility/ui/console/user_keyboard_input.h
@@ -1,7 +1,7 @@
 #ifndef __USER_KEYBOARD_INPUT_H__
 #define __USER_KEYBOARD_INPUT_H__
 
-#define MAX_USER_KEYBOARD_INPUT         4096
+#define MAX_USER_KEYBOARD_INPUT         128
 
 void get_user_keyboard_input(char *keyboard_input);
 void get_user_keyboard_input_with_message(char *output_message, char *keyboard_input);
