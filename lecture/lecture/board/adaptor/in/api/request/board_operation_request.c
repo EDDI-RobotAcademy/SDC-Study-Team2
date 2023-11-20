01782b811824ae502520706c9581f693faa404e2
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2023-11-20 17:26:13 +0900

    update: [조승훈] post_info에 입력받은 값 넣기 수정 [SDC-T2-x]

--- lecture/lecture/board/adaptor/in/api/request/board_operation_request.c
+++ lecture/lecture/board/adaptor/in/api/request/board_operation_request.c
@@ -14,34 +14,40 @@ post_info *init_post_info_by_write_command(void)
 {
     // 이 부분은 사용자로부터 입력 받거나 해야 함
     char keyboard_input_from_user[MAX_USER_KEYBOARD_INPUT] = { 0 };
+    post_info *tmp_post_info = (post_info *)malloc(sizeof(post_info));
+
     printf("제목을 입력하세요.\n");
     get_user_keyboard_input(keyboard_input_from_user);
-    char *title_from_keyboard = keyboard_input_from_user;
+    post_info_title_from_request(tmp_post_info, keyboard_input_from_user);
+    
     printf("작성자를 입력하세요.\n");
     get_user_keyboard_input(keyboard_input_from_user);
-    char *writer_from_keyboard = keyboard_input_from_user;
+    post_info_writer_from_request(tmp_post_info, keyboard_input_from_user);
+    
     printf("내용을 입력하세요.\n");
     get_user_keyboard_input(keyboard_input_from_user);
-    char *contents_from_keyboard = keyboard_input_from_user;
-
-    post_info *tmp_post_info = (post_info *)malloc(sizeof(post_info));
-    post_formation_by_request(tmp_post_info, title_from_keyboard, writer_from_keyboard, contents_from_keyboard);
-
+    post_info_contents_from_request(tmp_post_info, keyboard_input_from_user);
+    
     return tmp_post_info;
 }
 
-void post_formation_by_request(post_info *post_from_request, char *title, char *writer, char *contents)
+void post_info_title_from_request(post_info *post_from_request, char *title)
 {
     int title_length = strlen(title) + 1;
-    int writer_length = strlen(writer) + 1;
-    int contents_length = strlen(contents) + 1;
-
     post_from_request->post_title = (char *)malloc(sizeof(char) * title_length);
     strncpy(post_from_request->post_title, title, title_length);
+}
 
+void post_info_writer_from_request(post_info *post_from_request, char *writer)
+{
+    int writer_length = strlen(writer) + 1;
     post_from_request->post_writer = (char *)malloc(sizeof(char) * writer_length);
     strncpy(post_from_request->post_writer, writer, writer_length);
+}
 
+void post_info_contents_from_request(post_info *post_from_request, char *contents)
+{
+    int contents_length = strlen(contents) + 1;
     post_from_request->post_contents = (char *)malloc(sizeof(char) * contents_length);
     strncpy(post_from_request->post_contents, contents, contents_length);
 }
