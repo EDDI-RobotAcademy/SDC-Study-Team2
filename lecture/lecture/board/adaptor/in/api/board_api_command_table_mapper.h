2a6817c07b1f400f4e653c88c5823d45667045ea
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2023-11-20 14:26:44 +0900

    feat:[조승훈] board에서 구현할 api 고안하고, 가독성이 좋도록 명명합니다. [SDC-T2-3]

---
+++ lecture/lecture/board/adaptor/in/api/board_api_command_table_mapper.h
@@ -0,0 +1,11 @@
+#include __BOARD_API_COMMAND_TABLE_MAPPER_H__
+#define __BOARD_API_COMMAND_TABLE_MAPPER_H__
+
+#include "board_api_command.h"
+
+#define __BOARD_API_COMMAND_TABLE(number, function_prototype) \
+    [number] = function_prototype,
+
+__BOARD_API_COMMAND_TABLE(BOARD_API_OPERATION, post_operation)
+
+#endif
