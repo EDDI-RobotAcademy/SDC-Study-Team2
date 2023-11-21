d8293ce5e3dc61b7a77deeec1038eb369db27e96
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2023-11-21 09:09:17 +0900

    docs: [조승훈] 오류로 인한 lecture수정[SDC-x]

--- lecture/lecture/board/utility/ui/console/user_keyboard_input.h
+++
@@ -1,9 +0,0 @@
-#ifndef __USER_KEYBOARD_INPUT_H__
-#define __USER_KEYBOARD_INPUT_H__
-
-#define MAX_USER_KEYBOARD_INPUT         4096
-
-void get_user_keyboard_input(char *keyboard_input);
-void get_user_keyboard_input_with_message(char *output_message, char *keyboard_input);
-
-#endif
