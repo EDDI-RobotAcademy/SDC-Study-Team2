8cc71e2b9fd3b8c5369af883bdbc28032db4b956
crazyfire29 <57270706+crazyfire29@users.noreply.github.com>
2023-11-22 12:13:44 +0900

    docs: [조승훈] 오류로 인한 lecture 재 생성[SDC-x]

--- lecture/board/domain/model/board_model.c
+++ lecture/board/domain/model/board_model.c
@@ -3,9 +3,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-board_model *init_board_model_object(void)
+
+unsigned int test_form_unique_id;
+
+int increment_test_form_unique_id(void)
+{
+    return test_form_unique_id++;
+}
+
+board_model *init_board_model_object(char *title, char *author, char *content)
 {
     board_model *tmp_board = (board_model *)malloc(sizeof(board_model));
 
+    int title_length = strlen(title) + 1;
+    int author_length = strlen(author) +1;
+    int content_length = strlen(content) +1;
+
+    tmp_board->unique_id = increment_test_form_unique_id();
+    
+    tmp_board->title =
+        (char *)malloc(sizeof(char) * title_length);
+    strncpy(tmp_board->title , title , title_length);
+
+    tmp_board->author = 
+        (char *)malloc(sizeof(char) * author_length);
+    strncpy(tmp_board->author,author,author_length);
+
+    tmp_board->content = 
+        (char *)malloc(sizeof(char) * content_length);
+    strncpy(tmp_board->content,content,content_length);
+
+
+
     return tmp_board;
+
 }
