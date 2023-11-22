eb31d1313086f661f2f31b0244578a308bfacbd3
youngchan4 <149447336+youngchan4@users.noreply.github.com>
2023-11-22 15:00:01 +0900

    add: [황영찬] write에서 menu로 가는거 추가

--- youngchanhwang/test_board/board/application/service/post_write/post_write_service_command_handler.c
+++ youngchanhwang/test_board/board/application/service/post_write/post_write_service_command_handler.c
@@ -3,6 +3,12 @@
 #include "../../../domain/model/board_model.h"
 #include "../../../adapter/in/api/request/board_write_request.h"
 
+#include "../../../utility/save_in_text/save_data.h"
+
+
+#include "../board_service_command.h"
+#include "../board_service_command_table.h"
+
 #include <stdio.h>
 
 
@@ -13,4 +19,7 @@ void post_write_service(void)
 
     printf("%d\n%s%s%s\n",post_write_request->unique_id, post_write_request->title, post_write_request->author, post_write_request->content);
     
+    write_data_in_file(post_write_request);
+
+    board_service_command_table[POST_MENU_SERVICE_ON_BOARD_EXECUTE]();
 }
